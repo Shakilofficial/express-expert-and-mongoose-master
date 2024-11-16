db4e0b7247fb0c8351fe86ed7f974062ded7b32d
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-17 02:26:28 +0600

    complete module

--- module-4/src/app.ts
+++ module-4/src/app.ts
@@ -1,13 +1,14 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+//application routes
+
+app.use('/api/v1/students', StudentRoutes);
 
 export default app;
