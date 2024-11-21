f3f74a93bafa71ecc05fd32b395847617994df15
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-21 11:47:46 +0600

    project initialized with configure eslint,prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    ignores: ["node_modules", "dist"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "prettier/prettier": "error",
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
